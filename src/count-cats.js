e5507a31c44a0075057accc41a1853e6c7b4ec5a
Wad Werton <wad_werton@mail.ru>
2020-02-22 15:54:57 +0300

    feat: add solution task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let number = 0;
+  let cat = '^^';
+  for (let i = 0; i < backyard.length; i++){
+    for (let j = 0; j < backyard[i].length; j++){
+      if (backyard[i][j] === cat){
+        number += backyard[i][j];
+      }
+    }
+  }
+  return number;
 };
