e4968842ea8c8bcf77de65f088363861a20841d8
Wad Werton <wad_werton@mail.ru>
2020-02-23 18:30:23 +0300

    feat: add 2st solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,9 +1,10 @@
 module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length === 0) return false;
   let newArr = [];
 	for (let i = 0; i < members.length; i++){
-			if (typeof(members[i]) === 'object' || typeof(members[i]) === 'boolean' || typeof(members[i]) === 'number') continue;
-			//if (typeof(members[i][0]) === 'string') 
-			newArr.push(members[i][0]);
+			if (typeof(members[i]) === 'object' || typeof(members[i]) === 'boolean' || typeof(members[i]) === 'number' || typeof(members[i]) === 'undefined') continue;
+      let withoutSpacesMembers = members[i].trim();
+			newArr.push(withoutSpacesMembers[0]);
 	}
 	let string = newArr.join('').toUpperCase().split('').sort(function(a, b) {
 		return (a > b) ? 1 : -1;
