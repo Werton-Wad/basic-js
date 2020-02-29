9dd6f8b805226febfd474d6ced251b309efe08ad
Wad Werton <wad_werton@mail.ru>
2020-02-29 23:15:24 +0300

    feat: add solution task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (arguments.length === 0 || typeof sampleActivity !== 'string' || isNaN(parseFloat(sampleActivity))) return false;
+  if (sampleActivity <= 0 || sampleActivity > 15) return false;
+  sampleActivity = parseFloat(sampleActivity);
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.abs(Math.floor(Math.log(sampleActivity / MODERN_ACTIVITY) / k));
+  return t;
 };
